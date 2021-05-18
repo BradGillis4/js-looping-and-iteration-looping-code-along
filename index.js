b2fd824736c8a2975debb9840128093c185d1901
BradGillis4 <bradgillis4@gmail.com>
2021-05-18 17:33:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+
+
+
+function writeCards(names) {
+    return names.map(name =>  `Thank you, ${name}, for the wonderful surprise gift!`);
+  }
+
+  function countDown(number) {
+    for (let index = 0; index <= number; index++) {
+      console.log(index)
+    }
+  }
